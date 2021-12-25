28d157d63787c6d07713a9e30d24e23e3fc5a84d
flori <flori@DESKTOP-O2M2BOF.mshome.net>
2021-12-25 10:49:14 +0100

    mode 2 (flight phase dynamics) of single leg M-Slip model

---
+++ mode2.m
@@ -0,0 +1,26 @@
+function dx = mode2(t,x)
+%x = [x y phi theta dx dy dphi dtheta]
+% position x and y at center of entire Mass M along leg
+% phi: upper leg angle / theta: lower leg angle
+
+M = 80.;
+m_leg = .32*M;
+m_trunk = M-m_leg;
+L0 = 1;
+k_hip = ;
+d_hip = ;
+omega_ret = 50*pi/180;
+
+%leg retraction phi0 = α0 + ω · (t − tapex)
+phi0 = alpha0 + omega_ret * (t-t_apex);
+
+dx = [x(5);                                                 %dx
+    x(6);                                                   %dy
+    x(7);                                                   %dphi
+    x(8);                                                   %dtheta
+    0.;                                                     %ddx
+    -9.81;                                                  %ddy
+    (k_hip*(x(3)-phi0)+d_hip*(omega_ret-x(7)))/...          %ddphi
+    (m_leg*(m_trunk/M*.4*L0)^2+m_trunk*(mleg/M*.4*L0)^2);                    %
+    0.];                                                    %ddtheta
+end
