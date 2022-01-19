48123f8601e79ebdb20059c5a700e40df15ac891
Yanni Zhang <yanni.zhang@tum.de>
2022-01-19 11:32:25 +0100

    Replace main.m

--- main.m
+++ main.m
@@ -89,47 +89,69 @@ opti.subject_to(X(6,1) == X(6,N+1));
 
 
 %% Guard conditions
- 
-x_lo_knee = (X(1,N+1) - offset) + l_u * sin(X(3,N+1)); % lift off
+% l_l - length of lower leg
+% l_u - length of upper leg
+
+% lift off
+x_lo_knee = (X(1,N+1) - offset) + l_u * sin(X(3,N+1)); 
 y_lo_knee = X(2,N+1) - l_u * cos(X(3,N+1)) ;
+% constraint: length of lower leg equal to 0.6
+opti.subject_to(sqrt(x_lo_knee^2 + y_lo_knee^2) == l_l);  
 
-opti.subject_to(sqrt(x_lo_knee^2 + y_lo_knee^2) == l_l);
-
-y_td_knee = X(2,N_phase+1) - l_u * cos(X(3,N_phase+1)); % touch down    
+% touch down  
+y_td_knee = X(2,N_phase+1) - l_u * cos(X(3,N_phase+1));   
 angle_td_knee = X(3,N_phase+1) - X(4,N_phase+1);%%%%%%%%%%%%%%%%%%
-
+% constraint: height of knee computed from hip = height of knee computed from lower leg 
 opti.subject_to(y_td_knee == l_l * cos(angle_td_knee));
 
-%% Path contraiints
+% constraint: height of foot >= 0 at flight phase
+for i = 1:N_phase
+    opti.subject_to( (X(2,i) - l_u * cos(X(3,i)) - cos(X(3,i)-X(4,i)) * l_l) >= 0);
+end
 
-opti.subject_to(T_fl >= 0.15);
+% l_s - length of spring (lower leg)
+% actually l_s should equal to 0.6 since at N_phase+1 it hasn't shrunk yet
+l_s = sqrt((offset-(X(1,N_phase+1)+l1*sin(X(3,N_phase+1))))^2+(X(2,N_phase+1)-l1*cos(X(3,N_phase+1)))^2);
+
+% foot on the ground at N_phase+1 (y_foot = 0)
+opti.subject_to( (X(2,N_phase+1) - l_u * cos(X(3,N_phase+1)) - cos(X(3,N_phase+1)-X(4,N_phase+1)) * l_s) == 0);
+% foot start from the ground at time 1 (y_foot(time 1) = 0)
+opti.subject_to( (X(2,1) - l_u * cos(X(3,1)) - cos(X(3,1)-X(4,1)) * l_s) == 0);
+
+
+%% Path contraints
+% x = [x y phi theta dx dy dphi dtheta]
+opti.subject_to(T_fl >= 0.15);  
 opti.subject_to(T_st >= 0.15);
-opti.subject_to(X(2,:) >= 0.4);
-opti.subject_to(X(1,1) == 0.0);
-opti.subject_to(X(5,:) >= 0.0);
-opti.subject_to(X(3,:) >= -pi/4);
+% opti.subject_to(X(2,:) >= 0.4); % height of hip
+opti.subject_to(X(1,1) == 0.0); % starting point - horizontal coordinate x
+opti.subject_to(X(5,:) >= 0.0); % dx - hip keeps moving forward not backward
+opti.subject_to(X(3,:) >= -pi/4);   % phi 
 opti.subject_to(X(3,:) <= pi/4);
-
-opti.subject_to( X(2,:) - l_u * cos(X(3,:)) >= 0 ); % - l_l * cos(X(3,1:N_phase)+X(4,1:N_phase))
 
 
 
 %% Initial guess
+
+% for relaxed solution - without guard 
 
 % % opti.set_initial(X, repmat(x0,1,N+1));
 % opti.set_initial(X, zeros(8,N+1));
 % opti.set_initial(T_fl, 0.2);
 % opti.set_initial(T_st, 0.2);
 
+% for solution with guard
+
 % load('relaxed_solution_1.mat')
-load('strict_solution_2.mat')
+load('strict_solution_3.mat')
 opti.set_initial(X, x_sol);
 opti.set_initial(T_fl, time_fl);
 opti.set_initial(T_st, time_st);
 
+% show progress of optimization 
 opti.callback(@(i) plot(opti.debug.value(X(1,1:N+1)), opti.debug.value(X(2,1:N+1)),'b') )
 
-opti.solver('ipopt');
+opti.solver('ipopt');   % interior point method
 sol = opti.solve();
 
 %% Test & relaxed solution
@@ -144,7 +166,7 @@ sol = opti.solve();
 % Y_lo_knee = sol.value(X(2,N+1)) - l_u * cos(sol.value(X(3,N+1))) ;
 % sqrt(X_lo_knee^2 + Y_lo_knee^2)
 % % 
-% sol.value(offset)
+% % sol.value(offset)
 % 
 % time_fl = sol.value(T_fl)
 % time_st = sol.value(T_st)
@@ -152,14 +174,60 @@ sol = opti.solve();
 
 %% Plot 
 
-% x_hip_sol = sol.value(X(1,:));
-% y_hip_sol = sol.value(X(2,:));
-% phi_sol = sol.value(X(3,:));
-% theta_sol = sol.value(X(4,:));
+x_hip_sol = sol.value(X(1,:));
+y_hip_sol = sol.value(X(2,:));
+phi_sol = sol.value(X(3,:));
+theta_sol = sol.value(X(4,:));
+angle_sol = phi_sol - theta_sol;%%%%%%%%%%
+phi_td = sol.value(phi_sol(N_phase+1)); 
+theta_td = sol.value(theta_sol(N_phase+1)); 
+angle_td = sol.value(phi - theta);%%%%%%%%%%%
+offset_sol = x_hip_sol(N_phase+1) + l_u * sin(phi_td) + l_l * sin(angle_td);
+
+% Knee
+x_knee = zeros(1,N+1);
+y_knee = zeros(1,N+1);
+x_knee = x_hip_sol + l_u * sin(phi_sol);
+y_knee = y_hip_sol - l_u * cos(phi_sol);
+
+% Foot 
+x_foot = zeros(1,N+1);
+y_foot = zeros(1,N+1);
+l_s_sol = zeros(1,N+1);
+l_s_sol(1:N_phase) = 0.6;
+for i = N_phase+1:N+1
+    l_s_sol(i) = sqrt((offset_sol-(x_hip_sol(i)+l1*sin(phi_sol(i))))^2+(y_hip_sol(i)-l1*cos(phi_sol(i)))^2);
+end
+x_foot = x_knee + l_s_sol .* sin(angle_sol);
+y_foot = y_knee - l_s_sol .* cos(angle_sol);
+
+ 
+figure; hold on;
+for i = 1:N+1
+    plot(x_hip_sol(i), y_hip_sol(i),'o','Color','k');   % position of hip
+    plot(x_knee(i), y_knee(i),'o','Color','b'); % position of knee
+    plot(x_foot(i), y_foot(i),'o','Color','k');  % position of foot
+    plot([x_hip_sol(i),x_knee(i)],[y_hip_sol(i),y_knee(i)],'Color','k');    % upper leg
+    plot([x_knee(i),x_foot(i)],[y_knee(i),y_foot(i)],'Color','k');  % lower leg
+end
+plot(x_foot, y_foot,'-','Color','r');  %  foot
+plot(x_knee, y_knee,'-','Color','r');  %  knee
+plot(x_hip_sol, y_hip_sol,'-','Color','r');  %  hip
+
+% 
+% 
+%% Test 
+% 
+% 
+% load('strict_solution_3.mat')
+% x_hip_sol = x_sol(1,:);
+% y_hip_sol = x_sol(2,:);
+% phi_sol = x_sol(3,:);
+% theta_sol = x_sol(4,:);
 % angle_sol = phi_sol + theta_sol;
-% phi_td = sol.value(phi_sol(N_phase+1)); 
-% theta_td = sol.value(theta_sol(N_phase+1)); 
-% angle_td = sol.value(phi - theta);%%%%%%%%%%%
+% phi_td = phi_sol(N_phase+1); 
+% theta_td = theta_sol(N_phase+1); 
+% angle_td = phi - theta;%%%%%%%%%%%
 % offset_sol = x_hip_sol(N_phase+1) + l_u * sin(phi_td) + l_l * sin(angle_td);
 % 
 % % Knee
@@ -168,30 +236,19 @@ sol = opti.solve();
 % x_knee = x_hip_sol + l_u * sin(phi_sol);
 % y_knee = y_hip_sol - l_u * cos(phi_sol);
 % 
-% % Foot - flight
+% % Foot -
 % x_foot = zeros(1,N);
 % y_foot = zeros(1,N);
-% x_foot(1:N_phase) = x_knee(1:N_phase) + l_l * sin(angle_sol(1:N_phase));
-% y_foot(1:N_phase) = y_knee(1:N_phase) - l_l * cos(angle_sol(1:N_phase));
+% x_foot(1:N) = x_knee(1:N) + l_l * sin(angle_sol(1:N));
+% y_foot(1:N) = y_knee(1:N) - l_l * cos(angle_sol(1:N));
 % 
-% % Foot - stance
-% x_foot(N_phase+1:N) = offset_sol * ones(1,N_phase);
-% y_foot(N_phase+1:N) = zeros(1,N_phase);
 % 
-figure; hold on;
-for i = 1:N
-    plot(x_hip_sol(i), y_hip_sol(i),'o','Color','k');
-    plot(x_knee(i), y_knee(i),'o','Color','b');
-    plot(x_foot(i), y_foot(i),'o','Color','k');
-    plot([x_hip_sol(i),x_knee(i)],[y_hip_sol(i),y_knee(i)],'Color','k');
-    plot([x_knee(i),x_foot(i)],[y_knee(i),y_foot(i)],'Color','k');
-end
-%     
-%     
+% % 
 % figure; hold on;
 % for i = 1:N
 %     plot(x_hip_sol(i), y_hip_sol(i),'o','Color','k');
 %     plot(x_knee(i), y_knee(i),'o','Color','b');
-% %     plot(x_knee(i), phi_sol(i),'o','Color','b');
+%     plot(x_foot(i), y_foot(i),'o','Color','k');
+%     plot([x_hip_sol(i),x_knee(i)],[y_hip_sol(i),y_knee(i)],'Color','k');
+%     plot([x_knee(i),x_foot(i)],[y_knee(i),y_foot(i)],'Color','k');
 % end
-
