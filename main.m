fc761cd3db6ccb425411315f01ee4682cb819b82
Yanni Zhang <yanni.zhang@tum.de>
2022-01-27 16:09:19 +0100

    Replace main.m

--- main.m
+++ main.m
@@ -2,6 +2,7 @@ clc; clear all; close all;
 addpath('E:\CasADi\casadi-windows-matlabR2016a-v3.5.5')
 import casadi.*
 
+
 %% Initialization
 
 opti = casadi.Opti();
@@ -13,33 +14,36 @@ N = 80;
 N_phase = N/2;
 
 % x = [x y phi theta dx dy dphi dtheta]
-% control input : [dx 
-% alpha0 = opti.xariable(1)
-% omega = opti.variable(1)
+
 X = opti.variable(8, N+1);
 T_st = opti.variable(1);
 T_fl = opti.variable(1);
+% actuation parameters
+phi0 = opti.variable(1);
+alpha0 = opti.variable(1);
+omega = opti.variable(1);
+% control input
+U = opti.variable(1, N_phase);  % ddphi
 
 % Parameters
-global L0 l0 l1 M m_B m_L alpha0 g c_phi d1 c_theta d2 c1 t_apex omega;
+global L0 l0 l1 M m_B m_L g c_phi d1 c_theta d2 c1 t_apex;
 L0=1; M=80; g=9.81;
 l0 = .6*L0;                     %lower leg length
 l1 = .4*L0;                     %upper leg length
 l_l = l0;   % lower leg
 l_u = l1;   % upper leg
 m_L = .32*M;                    %leg mass
-m_B = M-m_L;                  %rest of body mass
-c_phi = 7500;                    %Nm/rad
+m_B = M-m_L;                    %rest of body mass
+c_phi = 750;                    %Nm/rad
 d1 = 2*sqrt(c_phi*m_L);
 c_theta = 1000;
 d2 = 2*sqrt(c_theta*m_L);
 c1 = 20 * 1000;
 
 c1 = 20*1000;
-alpha0 = (90 - 62) * pi/ 180.;
-t_apex = 0;
-omega = 50*pi/180; %rad/s
-x0 = [0.; 1; alpha0; 0; 6.; 0.; 0; 0];
+%alpha0 = (90 - 62) * pi/ 180.;
+t_apex = X(6,1);
+%omega = 50*pi/180; %rad/s
 
 %% Flight phase - Mode 2
 
@@ -49,30 +53,32 @@ h_fl = T_fl/N_phase;    % step width
 for k = 1:N_phase
     
     t = k * h_fl;
-    k1 = mode2(t, X(:,k));
-    k2 = mode2(t, X(:,k) + 0.5 * h_fl * k1);
-    k3 = mode2(t, X(:,k) + 0.5 * h_fl * k2);
-    k4 = mode2(t, X(:,k) + h_fl * k3);
+    k1 = mode2(t, X(:,k), alpha0, omega);
+    k2 = mode2(t, X(:,k) + 0.5 * h_fl * k1, alpha0, omega);
+    k3 = mode2(t, X(:,k) + 0.5 * h_fl * k2, alpha0, omega);
+    k4 = mode2(t, X(:,k) + h_fl * k3, alpha0, omega);
     
     x_next = X(:,k) + 1./6. * h_fl * (k1 + 2 * k2 + 2 * k3 + k4);
     
     opti.subject_to(X(:,k+1) == x_next);
 end
 
-phi = X(3,N_phase+1); theta = X(4,N_phase+1); angle = phi - theta;%%%%%%%%%%%%%
-offset = X(1,N_phase+1) + l_u * sin(phi) + l_l * sin(angle);
-% offset = X(1,N_phase+1) + L0*sin(X(3,N_phase+1));
+phi = X(3,N_phase+1);
+theta = X(4,N_phase+1);
+offset = X(1,N_phase+1) + l1 * sin(phi) + l0 * sin(phi - theta);
+
 %% Stance phase - Mode 1
 
 h_st = T_st/N_phase;    % step width
 
 % Runge-Kutta
 for k = N_phase+1:N
+    j = k-N_phase;
 
-    k1 = mode1(X(:,k), offset);
-    k2 = mode1(X(:,k) + 0.5 * h_st * k1, offset);
-    k3 = mode1(X(:,k) + 0.5 * h_st * k2, offset);
-    k4 = mode1(X(:,k) + h_st * k3, offset);
+    k1 = mode1(X(:,k), phi0, offset, U(j));
+    k2 = mode1(X(:,k) + 0.5 * h_st * k1, phi0, offset, U(j));
+    k3 = mode1(X(:,k) + 0.5 * h_st * k2, phi0, offset, U(j));
+    k4 = mode1(X(:,k) + h_st * k3, phi0, offset, U(j));
     
     x_next = X(:,k) + 1./6. * h_st * (k1 + 2 * k2 + 2 * k3 + k4);
     
@@ -87,69 +93,89 @@ opti.subject_to(X(3,1) == X(3,N+1));
 opti.subject_to(X(4,1) == X(4,N+1));
 opti.subject_to(X(5,1) == X(5,N+1));
 opti.subject_to(X(6,1) == X(6,N+1));
-% opti.subject_to(X(7,1) == X(7,N+1));
-% opti.subject_to(X(8,1) == X(8,N+1));
+opti.subject_to(X(7,1) == X(7,N+1));
+opti.subject_to(X(8,1) == X(8,N+1));
 
 
 %% Guard conditions
-% l_l - length of lower leg
-% l_u - length of upper leg
+% l0: length lower leg; l1: length upper leg
 
-% lift off
-x_lo_knee = (X(1,N+1) - offset) + l_u * sin(X(3,N+1)); 
-y_lo_knee = X(2,N+1) - l_u * cos(X(3,N+1)) ;
+% liftoff
+x_lo_knee = (X(1,N+1) - offset) + l1 * sin(X(3,N+1)); 
+y_lo_knee = X(2,N+1) - l1 * cos(X(3,N+1)) ;
 % constraint: length of lower leg equal to 0.6
-opti.subject_to(sqrt(x_lo_knee^2 + y_lo_knee^2) == l_l);  
+opti.subject_to(sqrt(x_lo_knee^2 + y_lo_knee^2) == l0);  
 
-% touch down  
-y_td_knee = X(2,N_phase+1) - l_u * cos(X(3,N_phase+1));   
-angle_td_knee = X(3,N_phase+1) - X(4,N_phase+1);%%%%%%%%%%%%%%%%%%
+% touchdown  
+y_td_knee = X(2,N_phase+1) - l1 * cos(X(3,N_phase+1));   
+alpha_td_knee = X(3,N_phase+1) - X(4,N_phase+1);%%%%%%%%%%%%%%%%%%
 % constraint: height of knee computed from hip = height of knee computed from lower leg 
-opti.subject_to(y_td_knee == l_l * cos(angle_td_knee));
+opti.subject_to(y_td_knee == l0 * cos(alpha_td_knee));
 
-% constraint: height of foot >= 0 at flight phase
-for i = 1:N_phase
-    opti.subject_to( (X(2,i) - l_u * cos(X(3,i)) - cos(X(3,i)-X(4,i)) * l_l) >= 0);
-end
+% % constraint: height of foot >= 0 at flight phase
+% for i = 1:N_phase
+%     opti.subject_to( (X(2,i) - l1 * cos(X(3,i)) - cos(X(3,i)-X(4,i)) * l0) >= 0);
+% end
 
-% l_s - length of spring (lower leg)
-% actually l_s should equal to 0.6 since at N_phase+1 it hasn't shrunk yet
-l_s = sqrt((offset-(X(1,N_phase+1)+l1*sin(X(3,N_phase+1))))^2+(X(2,N_phase+1)-l1*cos(X(3,N_phase+1)))^2);
+% % l_s - length of spring (lower leg)
+% % actually l_s should equal to 0.6 since at N_phase+1 it hasn't shrunk yet
+% l_s = sqrt((offset-(X(1,N_phase+1)+l1*sin(X(3,N_phase+1))))^2+(X(2,N_phase+1)-l1*cos(X(3,N_phase+1)))^2);
+% 
+% % foot on the ground at N_phase+1 (y_foot = 0)
+% opti.subject_to( (X(2,N_phase+1) - l1 * cos(X(3,N_phase+1)) - cos(X(3,N_phase+1)-X(4,N_phase+1)) * l_s) == 0);
+% % foot start from the ground at time 1 (y_foot(time 1) = 0)
+% opti.subject_to( (X(2,1) - l1 * cos(X(3,1)) - cos(X(3,1)-X(4,1)) * l_s) == 0);
 
-% foot on the ground at N_phase+1 (y_foot = 0)
-opti.subject_to( (X(2,N_phase+1) - l_u * cos(X(3,N_phase+1)) - cos(X(3,N_phase+1)-X(4,N_phase+1)) * l_s) == 0);
-% foot start from the ground at time 1 (y_foot(time 1) = 0)
-opti.subject_to( (X(2,1) - l_u * cos(X(3,1)) - cos(X(3,1)-X(4,1)) * l_s) == 0);
-
-
+% 
 %% Path contraints
 % x = [x y phi theta dx dy dphi dtheta]
 opti.subject_to(T_fl >= 0.15);  
 opti.subject_to(T_st >= 0.15);
-% opti.subject_to(X(2,:) >= 0.4); % height of hip
-opti.subject_to(X(1,1) == 0.0); % starting point - horizontal coordinate x
-opti.subject_to(X(5,:) >= 0.0); % dx - hip keeps moving forward not backward
-opti.subject_to(X(3,:) >= -pi/4);   % phi 
-opti.subject_to(X(3,:) <= pi/4);
 
+opti.subject_to(X(1,1) == 0); % starting point - horizontal coordinate x
+opti.subject_to(X(2,:) >= .4); % height of hip
+opti.subject_to(X(3,:) >= -pi*3/4);   % phi 
+opti.subject_to(X(3,:) <= pi*3/4);
+opti.subject_to(X(3,1) <= 0);
+opti.subject_to(X(4,:) >= 0);
+opti.subject_to(X(4,1) == 0);
+opti.subject_to(X(4,N_phase) == 0);
+opti.subject_to(X(5,:) >= 0); % dx - hip keeps moving forward not backward
+opti.subject_to(X(6,1) >= 0); % dy
 
+% new constraints including the actuation parameters as optimization
+% variables
+opti.subject_to(phi0 <= -pi/16);
+opti.subject_to(phi0 >= -pi/2);
+opti.subject_to(alpha0 <= pi/2);
+opti.subject_to(alpha0 >= -pi/2);
+opti.subject_to(omega >= 10*pi/180);
 
 %% Initial guess
-
-% for relaxed solution - without guard 
-
-% % opti.set_initial(X, repmat(x0,1,N+1));
-% opti.set_initial(X, zeros(8,N+1));
+% % 
+% % for relaxed solution - without guard 
+% x0 = [0.; .8; -35 * pi/ 180.; 0.; 5; .5; 0; 0];
+% opti.set_initial(X, repmat(x0,1,N+1));
+% opti.set_initial(phi0, -35*pi/180);
+% opti.set_initial(alpha0, -35*pi/180);
+% opti.set_initial(omega, 50*pi/180);
 % opti.set_initial(T_fl, 0.2);
 % opti.set_initial(T_st, 0.2);
+% opti.set_initial(U, zeros(1,N_phase));
+
+% restrict velocity
+
 
 % for solution with guard
 
-% load('relaxed_solution_1.mat')
-load('strict_solution_3.mat')
+load('relaxed_solution_controlled_4.mat')
+% load('relaxed_solution_2.mat')
+% load('strict_solution_5.mat')
+% load('kinda_strict_solution_controlled_1.mat')
 opti.set_initial(X, x_sol);
 opti.set_initial(T_fl, time_fl);
 opti.set_initial(T_st, time_st);
+opti.set_initial(U, zeros(1,N_phase));
 
 % show progress of optimization 
 opti.callback(@(i) plot(opti.debug.value(X(1,1:N+1)), opti.debug.value(X(2,1:N+1)),'b') )
@@ -171,9 +197,14 @@ sol = opti.solve();
 % % 
 % % sol.value(offset)
 % 
-% time_fl = sol.value(T_fl)
-% time_st = sol.value(T_st)
-% x_sol = sol.value(X)
+time_fl = sol.value(T_fl)
+time_st = sol.value(T_st)
+x_sol = sol.value(X)
+u_sol = sol.value(U)
+phi0_sol = sol.value(phi0)
+alpha0_sol = sol.value(alpha0)
+omega_sol = sol.value(omega)
+
 
 %% Plot 
 
