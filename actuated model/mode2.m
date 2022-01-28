738594ff5ad5763812eeb1ebec439cf5b04c2c82
flori <flori@DESKTOP-O2M2BOF.mshome.net>
2022-01-28 14:28:15 +0100

    _

--- actuated model/mode2.m
+++ actuated model/mode2.m
@@ -6,7 +6,7 @@ function dx = mode2Florian(t,x,alpha0,omega)
 global g m_L l1 c_phi c_theta d1 d2 t_apex;
 
 %leg retraction phi0 = α0 + ω · (t − tapex)
-phi0 = alpha0 + omega * (t-t_apex);     %t_apex still missing
+phi_0 = alpha0 + omega * (t-t_apex);     %t_apex still missing
 
 dx = [x(5);
     x(6);
@@ -14,6 +14,6 @@ dx = [x(5);
     x(8);    %-10*x(4); %test
     0.;
     -g;
-    (-c_phi*(x(3)-phi0)-d1*x(7)-m_L*g*sin(x(3))*l1)/(m_L*l1^2);
+    (-c_phi*(x(3)-phi_0)-d1*x(7)-m_L*g*sin(x(3))*l1)/(m_L*l1^2);
     0.];
 end
