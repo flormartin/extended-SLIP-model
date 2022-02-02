0080b9878bb936b0b0bbc02183c907dbb8e020dd
flori <flori@DESKTOP-O2M2BOF.mshome.net>
2022-02-02 11:07:17 +0100

    attempt to leave actuation out, not finished

--- unactuated model/run.m
+++ unactuated model/run.m
@@ -1,12 +1,12 @@
 clc; clear all; close all;
 
-global L0 l0 l1 M m_B m_L alpha0 g c_phi d1 c_theta d2 c1 offset t_apex omega theta dtheta;
+global L0 l0 l1 M m_B m_L g c_phi d1 c_theta d2 c1 offset t_apex;
 L0=1; M=80; g=9.81;
 l0 = .6*L0;                     %lower leg length
 l1 = .4*L0;                     %upper leg length
 m_L = .32*M;                    %leg mass
 m_B = M-m_L;                  %rest of body mass
-c_phi = 7500;                    %Nm/rad
+c_phi = 750;                    %Nm/rad
 d1 = 2*sqrt(c_phi*m_L);
 c_theta = 1000;
 d2 = 2*sqrt(c_theta*m_L);
@@ -15,25 +15,26 @@ c1 = 20 * 1000;
 
 c1 = 20*1000;
 alpha0 = (90 - 62) * pi/ 180.;
-t_apex = 0;
+phi0 = -35*pi/180;
 omega = 50*pi/180; %rad/s
-x0 = [0.; 1; alpha0; 0; 6.; 0.; 0; 0];
-
+x0 = [0.; .95; -35*pi/180; 3.5; 3; 0];
+t_apex = x0(5)/g;
 
 opts_flight = odeset('Events', @guard_flight);
 opts_stance = odeset('Events', @guard_stance);
 
 step_width = 0.001;
-tend = 10.;
+tend = 2.;
 tspan = 0:step_width:tend;
 
 x_full = [];
 t_full = [];
 
 % one flight phase and one stance phase
-[tout, xout, te, xe, ie] = ode45(@(t,x)mode2(t,x), tspan, x0, opts_flight);
+[tout, xout, te, xe, ie] = ode45(@(t,x)flight(t,x,alpha0,omega), tspan, x0, opts_flight);
+
 t_full = [t_full; tout + 0];
-x_full = [x_full; xout];
+x_full = [x_full; xout, zeros(size(xout,1),1)];
 y_full = [t_full; tout];
 
 offset = xe(1) + L0*sin(xe(3));
@@ -41,18 +42,21 @@ plot(offset,0,'x','Color','r','Markersize',15,'linewidth',2); hold on
 x0 = xe';
 tspan = te:step_width:tend;
 
-[tout, xout, te, xe, ie] = ode45(@(t,x)mode1(t,x), tspan, x0, opts_stance);
+[tout, xout, te, xe, ie] = ode45(@(t,x)stance(x,phi0,offset), tspan, x0, opts_stance);
+
 t_full = [t_full; tout + t_full(end)];
-x_full = [x_full; xout];
+theta = xout(:,3) - atan((offset-(xout(:,1)+sin(xout(:,3))*l1))./(xout(:,2)-cos(xout(:,3))*l1));
+x_full = [x_full; xout,theta];
 y_full = [t_full; tout];
 
 x0 = xe';
 tspan = te:step_width:tend;
-t_apex = te + xe(6)/g;
+t_apex = te + xe(5)/g;
 
-[tout, xout, te, xe, ie] = ode45(@(t,x)mode2(t,x), tspan, x0, opts_flight);
+[tout, xout, te, xe, ie] = ode45(@(t,x)flight(t,x,alpha0,omega), tspan, x0, opts_flight);
+
 t_full = [t_full; tout + 0];
-x_full = [x_full; xout];
+x_full = [x_full; xout, zeros(size(xout,1),1)];
 y_full = [t_full; tout];
 
 offset = xe(1) + L0*sin(xe(3));
@@ -60,29 +64,16 @@ plot(offset,0,'x','Color','r','Markersize',15,'linewidth',2); hold on
 x0 = xe';
 tspan = te:step_width:tend;
 
-[tout, xout, te, xe, ie] = ode45(@(t,x)mode1(t,x), tspan, x0, opts_stance);
+[tout, xout, te, xe, ie] = ode45(@(t,x)stance(x,phi0,offset), tspan, x0, opts_stance);
+
 t_full = [t_full; tout + t_full(end)];
-x_full = [x_full; xout];
+theta = xout(:,3) - atan((offset-(xout(:,1)+sin(xout(:,3))*l1))./(xout(:,2)-cos(xout(:,3))*l1));
+x_full = [x_full; xout,theta];
 y_full = [t_full; tout];
 
 x0 = xe';
 tspan = te:step_width:tend;
-t_apex = te + xe(6)/g;
-
-% [tout, xout, te, xe, ie] = ode45(@(t,x)mode2(t,x), tspan, x0, opts_flight);
-% t_full = [t_full; tout + 0];
-% x_full = [x_full; xout];
-% y_full = [t_full; tout];
-% 
-% offset = xe(1) + L0*sin(xe(3));
-% plot(offset,0,'x','Color','r','Markersize',15,'linewidth',2); hold on
-% x0 = xe';
-% tspan = te:step_width:tend;
-% 
-% [tout, xout, te, xe, ie] = ode45(@(t,x)mode1(t,x), tspan, x0, opts_stance);
-% t_full = [t_full; tout + t_full(end)];
-% x_full = [x_full; xout];
-% y_full = [t_full; tout];
+t_apex = te + xe(5)/g;
 
 %% plots
 
@@ -90,9 +81,9 @@ for i = 1:length(t_full)
     if mod(i,20) == 0
         plot(x_full(i,1), x_full(i,2),'o','Color','k')%hip
         plot(x_full(i,1)+l1*sin(x_full(i,3)), x_full(i,2)-l1*cos(x_full(i,3)),'o','Color','k')%knee
-        plot(x_full(i,1)+l1*sin(x_full(i,3))+l0*sin(x_full(i,3)-x_full(i,4)), x_full(i,2)-l1*cos(x_full(i,3))-l0*cos(x_full(i,3)-x_full(i,4)),'o','Color','k')%foot
+        plot(x_full(i,1)+l1*sin(x_full(i,3))+l0*sin(x_full(i,3)-x_full(i,7)), x_full(i,2)-l1*cos(x_full(i,3))-l0*cos(x_full(i,3)-x_full(i,7)),'o','Color','k')%foot
         plot([x_full(i,1),x_full(i,1)+l1*sin(x_full(i,3))],[x_full(i,2),x_full(i,2)-l1*cos(x_full(i,3))],'Color','k')
-        plot([x_full(i,1)+l1*sin(x_full(i,3)),x_full(i,1)+l1*sin(x_full(i,3))+l0*sin(x_full(i,3)-x_full(i,4))],[x_full(i,2)-l1*cos(x_full(i,3)),x_full(i,2)-l1*cos(x_full(i,3))-l0*cos(x_full(i,3)-x_full(i,4))],'Color','k')
+        plot([x_full(i,1)+l1*sin(x_full(i,3)),x_full(i,1)+l1*sin(x_full(i,3))+l0*sin(x_full(i,3)-x_full(i,7))],[x_full(i,2)-l1*cos(x_full(i,3)),x_full(i,2)-l1*cos(x_full(i,3))-l0*cos(x_full(i,3)-x_full(i,7))],'Color','k')
     end
 end
-figure;plot(x_full(:,4))
+% figure;plot(x_full(:,4))
